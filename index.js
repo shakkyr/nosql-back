517c845638d104d63e81fb419d19e4fc70df8b0c
Shadi Rada <shakkyr@gmail.com>
2022-06-07 02:38:22 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+require('dotenv').config();
 
 
 app.use(express.json())
@@ -21,8 +22,11 @@ app.use('/auth', usersRouter)
 
 
 db.sequelize.sync().then(()=> {
-    app.listen(3001, () => {
+    app.listen(process.env.PORT || 3001, () => {
         console.log('server runing on port 3001')
+    })
+    .catch(err => {
+        console.log(err);
     })
 
 })
